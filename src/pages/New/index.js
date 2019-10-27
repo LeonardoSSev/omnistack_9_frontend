f80c72a12f97e3c46c2346fdfa4febe9be0d4087
Leonardo Santos Severino <leonardossev@gmail.com>
2019-10-27 18:08:43 -0300

    Stores a new spot through the FormData

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,8 +1,9 @@
 import React, { useState, useMemo } from 'react';
+import api from '../../services/api';
 import camera from '../../assets/camera.svg';
 import './index.css'
 
-export default function New() {
+export default function New({ history }) {
   const [company, setCompany] = useState('');
   const [techs, setTechs] = useState('');
   const [price, setPrice] = useState('');
@@ -13,8 +14,22 @@ export default function New() {
       return thumbnail ? URL.createObjectURL(thumbnail) : null
     }, [thumbnail]);
 
-  function handleSubmit() {
-    
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData();
+
+    data.append('thumbnail', thumbnail);
+    data.append('company', company);
+    data.append('techs', techs);
+    data.append('price', price);
+
+    const user_id = localStorage.getItem('user');
+
+    await api.post('/spots', data, {
+      headers: { user_id }
+    });
+
+    history.push('/dashboard');
   }
 
   return (
