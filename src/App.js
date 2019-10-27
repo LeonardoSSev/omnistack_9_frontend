935f329c2e6961fdc3179f5682f7056546982424
Leonardo Santos Severino <leonardossev@gmail.com>
2019-10-27 03:54:38 -0300

    Calls sessions to start a new session

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import logo from './assets/logo.svg'
+import logo from './assets/logo.svg';
+import api from './services/api';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', {
+      email
+    });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
@@ -10,14 +25,16 @@ function App() {
         <p>
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
           <input
-            type="email"
             id="email"
+            type="email"
             placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
           />
-          <button class="btn" type="submit">Entra</button>
+          <button className="btn" type="submit">Entra</button>
         </form>
       </div>
     </div>
